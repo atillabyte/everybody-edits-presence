11965369c4c377e9b011fb64470b2f475ca95491
Atilla Lonny <contact.gh@atil.la>
2018-04-25 03:34:03 -0400

    in-game timestamp feature (tip: disable with no-timestamp command-line arg)

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
 const client = require('discord-rich-presence')('437773390363754497');
+const argv = require('minimist')(process.argv.slice(2));
 
 app.use(express.json());
 
 app.post("/", (request, response) => {
 	let body = request.body;
+	let timestamp = request.headers['drp-start-timestamp'];
+
 	let presence = {
 		state: body.state.substring(0, 128),
 		details: body.details.substring(0, 128),
 		largeImageKey: 'logo',
 		largeImageText: 'Everybody Edits',
 		instance: true
+	};
+
+	if (timestamp != -1 && !argv._.includes('no-timestamp')) {
+		presence.startTimestamp = Math.round(parseInt(timestamp) / 1000);
 	}
+
+	if (!presence.details)
+		return;
 
 	client.updatePresence(presence);
 	response.sendStatus(200);
--- extension/js/background.js
+++ extension/js/background.js
@@ -1,47 +1,76 @@
+var current_room_id = '';
+var current_room_name = '';
+var last_joined_world = 0;
+var last_update = new Date().getTime();
+
 function updatePresence(tabId, changeInfo, tab) {
+  chrome.tabs.query({}, function (tabs) {
+    var everybodyEditsInstances = tabs.some(function (tab) { return new URL(tab.url).hostname == 'everybodyedits.com' });
+
+    new URL('')
+    if (!everybodyEditsInstances) {
+      if (current_room_id != 'Offline')
+        setCurrentRoom('Not currently in a world.', 'Offline');
+    }
+  });
+
   if (tab.active === true && !(tab.url == undefined)) {
     var url = new URL(tab.url);
 
     if (url.hostname != 'everybodyedits.com')
       return;
 
-    var roomname_url = "games/";
-    var ee_roomid;
-    var ee_roomname;
+    var timestamp = performance.timing.navigationStart;
 
-    if (url.href.lastIndexOf(roomname_url) > -1) {
-      ee_roomid = unescape(url.href.substring(url.href.lastIndexOf(roomname_url) + roomname_url.length));
-      ee_roomname = tab.title.substring(0, tab.title.length - ' | Everybody Edits'.length);
+    var room_name_uri = "games/";
+    var room_id;
+    var room_name;
+
+    if (url.href.lastIndexOf(room_name_uri) > -1) {
+      room_id = unescape(url.href.substring(url.href.lastIndexOf(room_name_uri) + room_name_uri.length));
+      room_name = tab.title.substring(0, tab.title.length - ' | Everybody Edits'.length);
+
+      last_joined_world = Date.now();
     }
 
-    if (!ee_roomid)
-      ee_roomid = 'Lobby';
+    if (!room_id) {
+      room_id = 'Lobby';
+      room_name = 'Not currently in a world.';
+    }
 
-    if (!ee_roomname)
-      ee_roomname = 'Not currently in a world.';
+    var force_update = room_id != 'Lobby' && room_id != 'Offline' && (new Date().getTime() - last_update >= 5000);
 
-    var data = {
-      url: tab.url,
-      details: ee_roomname,
-      state: ee_roomid,
-      smallText: 'ST01',
-      largeText: 'Everybody Edits'
-    };
-
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "http://localhost:3000/",
-      "method": "POST",
-      "headers": {
-        "content-type": "application/json"
-      },
-      "processData": false,
-      "data": JSON.stringify(data)
-    };
-
-    $.ajax(settings);
+    if (room_id != current_room_id || room_name != current_room_name || force_update)
+      setCurrentRoom(room_name, room_id, timestamp + (last_joined_world - timestamp));
   }
+}
+
+function setCurrentRoom(room_name, room_id, tab_start_timestamp = -1) {
+  current_room_id = room_id;
+  current_room_name = room_name;
+  last_update = new Date().getTime();
+
+  if (room_id == 'Lobby' || room_id == 'Offline')
+    tab_start_timestamp = -1;
+
+  $.ajax({
+    "async": true,
+    "crossDomain": true,
+    "url": "http://localhost:3000/",
+    "method": "POST",
+    "headers": {
+      "content-type": "application/json",
+      "drp-start-timestamp": tab_start_timestamp.toString()
+    },
+    "processData": false,
+    "data": JSON.stringify({
+      url: 'https://everybodyedits.com/games/' + room_id,
+      details: room_name,
+      state: room_id,
+      smallText: 'Everybody Edits',
+      largeText: 'Everybody Edits'
+    })
+  });
 }
 
 chrome.tabs.onUpdated.addListener(updatePresence);
